bf460677d7d1d995e9b764484b108da3ee6c4f1a
risamatsumoto1104 <risashibata114@gmail.com>
2024-10-26 17:10:27 +0900

    バリデーションの実装の練習

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
 use App\Models\Author; // Eloquentを使用できるようにモデルを読み込む
 
 class AuthorController extends Controller
@@ -24,7 +25,7 @@ class AuthorController extends Controller
 
     // 3.データ追加機能の設定
     // createアクションの引数にRequest $requestを指定することでリクエストボディを受け取る
-    public function create(Request $request)
+    public function create(AuthorRequest $request)
     {
         // リクエストボディから全ての情報を取得する
         $form = $request->all();
--- src/resources/views/add.blade.php
+++ src/resources/views/add.blade.php
@@ -9,17 +9,38 @@
 {{-- @endsection --}}
 
 @section('content')
+@if (@session('message'))
+<p>入力に問題があります</p>
+@endif
 <form action="/add" method="post">
 @csrf
     <table>
+        @error('name')
+        <tr>
+            <th style="background-color:red">ERROR</th>
+            <td>{{$errors->first('name')}}</td>
+        </tr>
+        @enderror
         <tr>
             <th>name</th>
             <td><input type="text" name="name"></td>
         </tr>
+        @error('name')
+        <tr>
+            <th style="background-color:red">ERROR</th>
+            <td>{{$errors->first('age')}}</td>
+        </tr>
+        @enderror
         <tr>
             <th>age</th>
             <td><input type="text" name="age"></td>
         </tr>
+        @error('name')
+        <tr>
+            <th style="background-color:red">ERROR</th>
+            <td>{{$errors->first('nationality')}}</td>
+        </tr>
+        @enderror
         <tr>
             <th>nationality</th>
             <td><input type="text" name="nationality"></td>
